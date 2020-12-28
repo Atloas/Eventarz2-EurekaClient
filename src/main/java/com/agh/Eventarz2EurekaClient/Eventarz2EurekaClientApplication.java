51316444486de2d9ba084eac684ad77644218e8d
Atloas <miter010@gmail.com>
2020-12-28 22:31:47 +0100

    fixed

--- src/main/java/com/agh/Eventarz2EurekaClient/Eventarz2EurekaClientApplication.java
+++ src/main/java/com/agh/Eventarz2EurekaClient/Eventarz2EurekaClientApplication.java
@@ -5,27 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaClient
+@RestController
 @SpringBootApplication
 public class Eventarz2EurekaClientApplication implements GreetingController {
 
-	@Autowired
-	@Lazy
-	private EurekaClient eurekaClient;
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
 
-	@Value("${spring.application.name}")
-	private String appName;
+    @Value("${spring.application.name}")
+    private String appName;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Eventarz2EurekaClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Eventarz2EurekaClientApplication.class, args);
+    }
 
-	@Override
-	public String greeting() {
-		return String.format(
-				"Hello from '%s'!", eurekaClient.getApplication(appName).getName());
-	}
+    @Override
+    public String greeting() {
+        return String.format(
+                "Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+    }
 }
